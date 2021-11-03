7fdc4fb817bb3a1a918f10f2bdc6211f0040f3ad
Diane <64610122+DianeHemi@users.noreply.github.com>
2021-11-03 11:48:15 +0100

    Update iterator.hpp

--- iterator.hpp
+++ iterator.hpp
@@ -11,6 +11,21 @@
 
 namespace ft
 {
+    template<class Iterator>
+    class iterator_traits
+    {
+        public:
+            typedef typename Iterator::iterator_category    iterator_category;
+            typedef typename Iterator::value_type           value_type;
+            typedef typename Iterator::difference_type      difference_type;
+            typedef typename Iterator::pointer              pointer;
+            typedef typename Iterator::reference            reference;
+    };
+    
+    
+    
+    
+    
     template<
         class Category,                     //Category of the iterator
         class T,                            //Type of valuesthat can be obtained by dereferencing the iterator
