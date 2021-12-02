26186e3e5f0231d14868a19b8d82d48d00ef3feb
DianeHemi <dchampda@student.42.fr>
2021-12-02 17:53:13 +0100

    Started working on set main

--- set/main.cpp
+++ set/main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
     ft::set<int> s;
+    std::cout << "First set created. Empty ? " << s.empty() << " | Max size : " << s.max_size() << std::endl;
 
     s.insert(12);
     s.insert(8);
@@ -17,14 +18,15 @@ int main()
     ft::set<int>::iterator ite = s.end();
     ite--;
     it++;
-    std::cout << "Insert" << std::endl;
+    std::cout << "\nInsert" << std::endl;
     std::cout << "Iterators : " << *it << " " << *ite << std::endl;
-    std::cout << "Size : " << s.size() << std::endl;
+    std::cout << "Size : " << s.size() << " | Empty ? " << s.empty() << std::endl;
 
 
     ft::set<int> s2(s);
     it = s2.begin();
     ite = s2.end();
+    ite--;
     std::cout << "Copy construct - Iterators : " << *it << " " << *ite << std::endl;
     std::cout << "Size : " << s2.size() << std::endl;
 
@@ -59,7 +61,24 @@ int main()
     std::cout << "Iterators : " << *it2 << " " << *ite2 << std::endl;
     std::cout << "Size : " << s3.size() << std::endl;
 
+    std::cout << "\nSwap" << std::endl;
+    s3.swap(s2);
+    it2 = s3.begin();
+    ite2 = s3.end();
+    ite2--;
+    std::cout << "Iterators : " << *it2 << " " << *ite2 << std::endl;
+    std::cout << "Size : " << s3.size() << std::endl;
 
+    //equal range, lower bound, upper bound, clear
+    //Operators
+
+    std::cout << "\nCount of keys" << std::endl;
+    std::cout << "32 : " << s3.count(32) << " | 11 : " << s3.count(11) << std::endl;
+
+    std::cout << "\nFind" << std::endl;
+    std::cout << "Find 9 : " << *s3.find(9) << " | 12 : " << *s3.find(12) << std::endl;
+
+    
 
     return 0;
 }
